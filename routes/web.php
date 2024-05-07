dbefb7d7c6d1e16ea112bc067e44a4caedb80ab1
Bootris <borisboncic95@gmail.com>
2024-05-07 15:09:47 +0200

    feat(update-delete-job):added update and delete routes for job

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
 
     return view('home');
 });
-
+// Index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->latest()->paginate(5);
 
@@ -15,22 +15,22 @@ Route::get('/jobs', function () {
         'jobs' => $jobs,
     ]);
 });
-
+// Create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
-
+// Show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
 });
-
+// Store
 Route::post('/jobs', function () {
     // validation...
     request()->validate([
-        'title'=> ['required', 'min:3'],
-        'salary'=> 'required',
+        'title' => ['required', 'min:3'],
+        'salary' => 'required',
     ]);
 
     Job::create([
@@ -38,6 +38,45 @@ Route::post('/jobs', function () {
         'salary' => request('salary'),
         'employer_id' => 1,
     ]);
+
+    return redirect('/jobs');
+});
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+// Update
+Route::patch('/jobs/{id}/', function ($id) {
+    //validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => 'required',
+    ]);
+    //authorize
+
+    // update the job
+    $job = Job::findOrFail($id);
+
+    // $job->title = request('title');
+    // $job->salary = request('salary');
+    // $job->save();
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    // and persist
+    // redirect to the job page
+    return redirect('/jobs/'. $job->id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}/', function ($id) {
+    $job = Job::findOrFail($id);
+    $job->delete();
 
     return redirect('/jobs');
 });
