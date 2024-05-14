5904703279de35c287b9cadaf10ea2d66d990774
Bootris <borisboncic95@gmail.com>
2024-05-14 14:41:03 +0200

    feat(authorize):added the authorize

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Job;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,13 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'John',
+            'last_name' => 'Doe',
             'email' => 'test@example.com',
         ]);
 
-        Job::factory(100)->create();
+        $this->call(JobSeeder::class);
     }
 }
--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -8,9 +8,10 @@
     <p>
         this job pays {{ $job->salary }} per year.
     </p>
+    @can('edit', $job)
+        <p class="mt-6">
+            <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
 
-    <p class="mt-6">
-        <x-button href="/jobs/{{ $job->id }}/edit">Edit Job</x-button>
-
-    </p>
+        </p>
+    @endcan
 </x-layout>
