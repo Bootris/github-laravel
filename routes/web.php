923b62edcea67c64d0c7e9bf2ae794c0def67919
Bootris <borisboncic95@gmail.com>
2024-05-15 12:15:36 +0200

    feat(queue):added queue

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,18 @@
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
+use App\Jobs\TranslateJob;
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
+
+Route::get('test', function () {
+    $job = Job::first();
+    TranslateJob::dispatch($job);
+
+    return 'Done';
+});
 
 Route::controller(JobController::class)->group(function () {
     Route::get('/jobs', 'index');
